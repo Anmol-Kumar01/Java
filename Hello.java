133cce313c7ca6f2e9fdde7098bc46e7b4ae1892
Anmol-Kumar01 <anmol888236@gmail.com>
2024-08-06 23:18:06 +0530

    Mid Commit

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String [] args) {
         System.out.println("Hello World");
         System.out.println("This is my first java program");
-        System.out.println("Welcome to this world");
+        System.out.println("Welcome to this world of Java Programing");
+
     }    
 }
